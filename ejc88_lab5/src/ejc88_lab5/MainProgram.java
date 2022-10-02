d1b48f9b1f882fa49bcb60ee5f2fc892f7108fc4
Cheggrett <112407004+Cheggrett@users.noreply.github.com>
2022-10-02 19:44:23 -0400

    Update MainProgram.java

--- ejc88_lab5/src/ejc88_lab5/MainProgram.java
+++ ejc88_lab5/src/ejc88_lab5/MainProgram.java
@@ -8,23 +8,23 @@ public class MainProgram {
 	public static void main(String[] args) {
 		Random dice = new Random();
 		Scanner scan = new Scanner(System.in);
-		
+
 		String loop = "Yes";
-do {
-		System.out.println("How many times would you like to roll the dice?");
-		String rollSTR = scan.nextLine();
-		int rolls = Integer.parseInt(rollSTR);
-	
-		
-		rollDice(rolls,dice);
-		
-		System.out.println("would you like to go again? enter Yes or No");
-		 loop = scan.nextLine();
-		
-}while( loop.equals("Yes"));
-	
+		do {
+			System.out.println("How many times would you like to roll the dice?");
+			String rollSTR = scan.nextLine();
+			int rolls = Integer.parseInt(rollSTR);
+
+
+			rollDice(rolls,dice);
+
+			System.out.println("would you like to go again? enter Yes or No");
+			loop = scan.nextLine();
+
+		}while( loop.equals("Yes"));
+
 	}
-//method to roll dice
+	//method to roll dice
 	static void rollDice(int rolls, Random dice) {
 		//declares local variables for counting
 		double val2 = 0;
@@ -38,10 +38,10 @@ do {
 		double val10 = 0;
 		double val11 = 0;
 		double val12 = 0;
-		
-//loop rolls two dice with random numbers from 0-5, adds 1, will repeat the roll as many times
-//as specified from main function'
-		
+
+		//loop rolls two dice with random numbers from 0-5, adds 1, will repeat the roll as many times
+		//as specified from main function'
+
 		for (int i = 0; i <= rolls; i++) {
 			int roll1 = dice.nextInt(6);
 			roll1 = roll1 + 1;
@@ -49,9 +49,9 @@ do {
 			roll2 = roll2 + 1;
 
 			int total = roll1 + roll2;
-			
-//adds the newly generated value to the count
-			
+
+			//adds the newly generated value to the count
+
 			if (total == 2) {
 				val2 = val2 + 1;
 			} else if (total == 3) {
@@ -78,18 +78,7 @@ do {
 
 			}
 		}
-		/**System.out.println(val2);
-		System.out.println(val3);
-		System.out.println(val4);
-		System.out.println(val5);
-		System.out.println(val6);
-		System.out.println(val7);
-		System.out.println(val8);
-		System.out.println(val9);
-		System.out.println(val10);
-		System.out.println(val11);
-		System.out.println(val12);
-		**/
+
 		//divides counts by 100 to get fractions
 		val2 = val2/rolls;
 		val3 = val3/rolls;
@@ -102,7 +91,7 @@ do {
 		val10 = val10/rolls;
 		val11 = val11/rolls;
 		val12 = val12/rolls;
-		
+
 		//displays all the newly calculated values
 		System.out.println(val2);
 		System.out.println(val3);
