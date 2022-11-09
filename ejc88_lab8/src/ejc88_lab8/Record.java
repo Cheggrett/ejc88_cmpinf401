c7016b45cd2b32ee8046b87adfd735a2ae8576c7
Cheggrett <112407004+Cheggrett@users.noreply.github.com>
2022-11-09 14:29:29 -0500

    Lab 8

---
+++ ejc88_lab8/src/ejc88_lab8/Record.java
@@ -0,0 +1,19 @@
+package ejc88_lab8;
+
+public class Record {
+	private String name;
+	private double time;
+	
+	public Record(String name, double time) {
+		this.name = name;
+		this.time = time;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public double getTime() {
+		return this.time;
+	}
+}
